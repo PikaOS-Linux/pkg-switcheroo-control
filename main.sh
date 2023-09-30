563275711c8ed5321a2743749003bae5b9ed1d89
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-09-30 11:05:34 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,9 +3,11 @@
 DEBIAN_FRONTEND=noninteractive
 
 # Clone Upstream
-mkdir -p ./src-pkg-name
-cp -rvf ./debian ./src-pkg-name/
-cd ./src-pkg-name/
+git clone https://gitlab.freedesktop.org/hadess/switcheroo-control -b 2.6
+cp -rvf ./debian ./switcheroo-control/
+cd ./switcheroo-control
+
+for i in ../patches/* ; do echo "Applying Patch: $i" && patch -Np1 -i $i || echo "Applying Patch $i Failed!"; done
 
 # Get build deps
 apt-get build-dep ./ -y
